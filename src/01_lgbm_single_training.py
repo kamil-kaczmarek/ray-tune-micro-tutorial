b7765c502ccbee93800665ab38cc7b73f7d7b1b3
Kamil A. Kaczmarek <kaczmarek.poczta@gmail.com>
2022-08-03 18:11:46 -0700

    use digits, trains in single iterations

--- src/01_lgbm_single_training.py
+++ src/01_lgbm_single_training.py
@@ -1,41 +1,45 @@
 import lightgbm as lgb
 import numpy as np
-from sklearn.datasets import load_wine
+from sklearn.datasets import load_digits
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 # Prepare dataset
-X, y = load_wine(return_X_y=True)
-X_train, X_valid, y_train, y_valid = train_test_split(
-    X, y, test_size=0.2, random_state=7707, stratify=y
-)
+X, y = load_digits(return_X_y=True)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=7707)
 
-train_data = lgb.Dataset(data=X_train, label=y_train)
-valid_data = lgb.Dataset(data=X_valid, label=y_valid, reference=train_data)
+train_data = lgb.Dataset(data=X_train, label=y_train, free_raw_data=False)
+valid_data = lgb.Dataset(data=X_valid, label=y_valid, free_raw_data=False, reference=train_data)
 
 # Set training parameters for single training run
 training_parameters = {
     "objective": "multiclass",
     "metric": "multi_logloss",
-    "num_class": 3,
-    "num_leaves": 7,
-    "learning_rate": 0.1,
-    "feature_fraction": 0.9,
-    "bagging_fraction": 0.8,
-    "bagging_freq": 1,
-    "max_depth": 5,
+    "num_class": 10,
+    "num_leaves": 5,
+    "learning_rate": 0.001,
+    "feature_fraction": 0.5,
+    "bagging_fraction": 0.5,
+    "bagging_freq": 100,
+    "max_depth": 2,
     "verbose": -1,
 }
 
-# Train LightGBM model and log results to stdout
-gbm = lgb.train(
-    params=training_parameters,
-    train_set=train_data,
-    num_boost_round=10,
-    valid_sets=[train_data, valid_data],
-    valid_names=["train", "valid"],
-    callbacks=[lgb.log_evaluation(period=10)],
-)
+# Initialize booster
+gbm = lgb.Booster(params=training_parameters, train_set=train_data)
+
+# Train booster for 200 iterations
+for i in range(200):
+    gbm = lgb.train(
+        init_model=gbm,
+        params=training_parameters,
+        train_set=train_data,
+        num_boost_round=1,
+        valid_sets=[train_data, valid_data],
+        valid_names=["train", "valid"],
+        callbacks=[lgb.log_evaluation(period=100)],
+        keep_training_booster=True,
+    )
 
 # Print accuracy on validation data
 y_pred = np.argmax(gbm.predict(X_valid), axis=1)
